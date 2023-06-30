d2749c565eefeb383dcad513cb4b724b5371ec68
Boiko739 <boiko.ie739@gmail.com>
2023-07-01 00:02:52 +0300

    Small edits

--- Assets/Scripts/PipeMoveScript.cs
+++ Assets/Scripts/PipeMoveScript.cs
@@ -2,19 +2,26 @@ using UnityEngine;
 
 public class PipeMoveScript : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float deadZone = -14f;
+    private LogicScript _logic;
+
+    private float _pipeMoveSpeed = 10f;
+
+    public float PipeMoveSpeed { get => _pipeMoveSpeed; set => _pipeMoveSpeed = value; }
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-
+        _logic = GameObject.FindWithTag("Logic").GetComponent<LogicScript>();
     }
-
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        transform.position += (Vector3.left * moveSpeed) * Time.deltaTime;
-        if (transform.position.x < deadZone)
+        transform.position += Vector3.left * PipeMoveSpeed * Time.deltaTime;
+        if (transform.position.x < _logic.DeadZone)
             Destroy(gameObject);
+    }
+    public void IncreaseSpeed(LogicScript ls)
+    {
+        GetComponent<PipeMoveScript>().PipeMoveSpeed += ls.ScoreToAdd / 10f;
     }
 }
