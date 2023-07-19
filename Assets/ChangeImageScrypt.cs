1f7f08c01b3df39e5a8c751c419816d09df5375d
Boiko739 <boiko.ie739@gmail.com>
2023-07-19 23:33:15 +0300

    Hat image changing
    trying to implement more flexible system

---
+++ Assets/ChangeImageScrypt.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class ChangeImageScrypt : MonoBehaviour
+{
+    public GameObject customizationManager;
+
+    private void NextImage()
+    {
+        //      gameObject.GetComponent<Image>().sprite = (Sprite)Resources.Load($"Sprites//Hats//{customizationManager.GetNextImagePath()}");
+    }
+}
