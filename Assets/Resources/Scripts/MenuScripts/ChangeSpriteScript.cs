2dee8e757943e3e10faa0b566ad37ad5efce4b87
Boiko739 <boiko.ie739@gmail.com>
2023-07-28 20:28:27 +0300

    Arch desition
    I separated the responsibility of changing the sprites from every gameObject and gave it to the SpritesManager

--- Assets/Resources/Scripts/MenuScripts/ChangeSpriteScript.cs
+++ Assets/Resources/Scripts/MenuScripts/ChangeSpriteScript.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class ChangeSpriteScript : MonoBehaviour
 {
     public int currentHatSprite = 0, currentBirdSprite = 0;
+    public GameObject bird, hat;
+    [SerializeField]
     private List<Sprite> _hatSprites, _birdSprites;
 
     private void Start()
@@ -30,28 +32,28 @@ public class ChangeSpriteScript : MonoBehaviour
     }
     public void ChangeHatSprite(int next)
     {
-        currentHatSprite = ChangeSprite(currentHatSprite, next, _hatSprites);
+        currentHatSprite = ChangeSprite(hat, currentHatSprite, next, _hatSprites);
     }
     public void ChangeBirdSprite(int next)
     {
-        currentBirdSprite = ChangeSprite(currentBirdSprite, next, _birdSprites);
+        currentBirdSprite = ChangeSprite(bird, currentBirdSprite, next, _birdSprites);
     }
-    private int ChangeSprite(int currentSprite, int next, List<Sprite> sprites)
+    private int ChangeSprite(GameObject objToChange, int currentSprite, int next, List<Sprite> sprites)
     {
         int nextSprite = currentSprite + next;
         int lastSpriteIndex = sprites.Count - 1;
 
         if (nextSprite < 0)
         {
-            gameObject.GetComponent<Image>().sprite = sprites[lastSpriteIndex];
+            objToChange.GetComponent<Image>().sprite = sprites[lastSpriteIndex];
             return lastSpriteIndex;
         }
         else if (nextSprite > lastSpriteIndex)
         {
-            gameObject.GetComponent<Image>().sprite = sprites[0];
+            objToChange.GetComponent<Image>().sprite = sprites[0];
             return 0;
         }
-        gameObject.GetComponent<Image>().sprite = sprites[nextSprite];
+        objToChange.GetComponent<Image>().sprite = sprites[nextSprite];
 
         return nextSprite;
     }
