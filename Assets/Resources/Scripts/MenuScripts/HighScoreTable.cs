1979942ef3b3d9a9d89f6ff719f60cce40727b40
Boiko739 <boiko.ie739@gmail.com>
2023-08-02 21:18:17 +0300

    Update HighScoreTable.cs
    It took too much time to finish I think. However, I am satisfied with the result.

--- Assets/Resources/Scripts/MenuScripts/HighScoreTable.cs
+++ Assets/Resources/Scripts/MenuScripts/HighScoreTable.cs
@@ -1,79 +1,128 @@
 using System;
 using System.Collections.Generic;
-using UnityEditor.Build.Pipeline;
-using UnityEditor.Localization.Plugins.XLIFF.V12;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HighScoreTable : MonoBehaviour
+public class HighscoreTable : MonoBehaviour
 {
     private Transform _entryContainer, _entryTemplate;
     private List<HighscoreEntry> _highscoreEntryList;
-    private List<Transform> _highScoreEntryTransformList;
+    private List<Transform> _highscoreEntryTransformList;
 
 
     private void Awake()
     {
-        _entryContainer = transform.Find("HighScoreEntryContainer");
-        _entryTemplate = _entryContainer.Find("HighScoreEntryTemplate");
+        _entryContainer = transform.Find("HighscoreEntryContainer");
+        _entryTemplate = _entryContainer.Find("HighscoreEntryTemplate");
 
         _entryTemplate.gameObject.SetActive(false);
 
-        _highscoreEntryList = new List<HighscoreEntry>()
+        _highscoreEntryList = LoadHighscores();
+        if (_highscoreEntryList == null)
+            return;
+
+        SortEntries();
+       
+        ShowEntries(_highscoreEntryList);
+        
+        if (_highscoreEntryList.Count >= 6)
+            SaveHighscores(_highscoreEntryList.GetRange(0, 6));
+        else SaveHighscores(_highscoreEntryList);
+    }
+
+    private void ShowEntries(List<HighscoreEntry> highscoreEntries)
+    {
+        _highscoreEntryTransformList = new List<Transform>();
+        short ind = 0;
+        foreach (var highscoreEntry in highscoreEntries)
         {
-            new HighscoreEntry{score = 3245, Date = "12/23/2023"},
-            new HighscoreEntry{score = 2340, Date = "10/19/2023"},
-            new HighscoreEntry{score = 3043, Date = "11/20/2023"},
-            new HighscoreEntry{score = 2450, Date = "01/08/2023"},
-            new HighscoreEntry{score = 2950, Date = "14/19/2023"},
-            new HighscoreEntry{score = 10000, Date = DateTime.Now.ToString()}
-        };
-        for (var i = 0; i < _highscoreEntryList.Count; i++)
+            if (ind == 6)
+                break;
+            CreateHighscoreEntryTransform(highscoreEntry, _entryContainer, _highscoreEntryTransformList);
+            ind++;
+        }
+    }
+
+    private void SortEntries()
+    {
+        for (int i = 0; i < _highscoreEntryList.Count; i++)
         {
-            for (int j = 0; j < _highscoreEntryList.Count; j++)
+            for (int j = i + 1; j < _highscoreEntryList.Count; j++)
             {
-                if (_highscoreEntryList[i].score > _highscoreEntryList[j].score)
+                if (_highscoreEntryList[j].score > _highscoreEntryList[i].score)
                 {
                     //Swap 
-                    var tmp = _highscoreEntryList[j];
-                    _highscoreEntryList[j] = _highscoreEntryList[i];
-                    _highscoreEntryList[i] = tmp;
+                    var tmp = _highscoreEntryList[i];
+                    _highscoreEntryList[i] = _highscoreEntryList[j];
+                    _highscoreEntryList[j] = tmp;
                 }
             }
         }
-
-        var highscoreEntryTransformList = new List<Transform>();
-        foreach (HighscoreEntry highscoreEntry in _highscoreEntryList)
-            CreateHighScoreEntryTransform(highscoreEntry, _entryContainer, highscoreEntryTransformList);
-
-        PlayerPrefs.SetString("highscoreTable","");
-        PlayerPrefs.Save();
-        PlayerPrefs.GetString("highscoreTable");
     }
-    private void CreateHighScoreEntryTransform(HighscoreEntry highScoreEntry, Transform container, List<Transform> transformlList)
+
+    private void SaveHighscores(List<HighscoreEntry> highscoreEntries)
+    {
+        _highscoreEntryList = highscoreEntries;
+        string json = JsonUtility.ToJson(new Highscores(highscoreEntries));
+        PlayerPrefs.SetString("highscoreTable", json);
+        PlayerPrefs.Save();
+    }
+
+    private List<HighscoreEntry> LoadHighscores()
+    {
+        return JsonUtility.FromJson<Highscores>(PlayerPrefs.GetString("highscoreTable")).highscoreEntryList;
+    }
+
+    public void AddHighscoreEntry(int score)
+    {
+        var highscoreEntries = LoadHighscores() ?? new List<HighscoreEntry>();
+        var date = DateTime.Now.ToShortDateString()[..6] +
+            DateTime.Now.Year.ToString()[2..]; //gives dd.mm.yy
+        highscoreEntries.Add(new HighscoreEntry { score = score, Date = date });
+        SaveHighscores(highscoreEntries);
+    }
+    private void CreateHighscoreEntryTransform(HighscoreEntry highScoreEntry, Transform container, List<Transform> transformlList)
     {
         short templateHeight = 72;
 
         Transform entryTransform = Instantiate(_entryTemplate, container);
-        RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
-        entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * transformlList.Count);
+        entryTransform.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -templateHeight * transformlList.Count);
         entryTransform.gameObject.SetActive(true);
 
         short rank = (short)(transformlList.Count + 1);
         entryTransform.Find("PositionEntry").GetComponent<Text>().text = rank.ToString();
 
-        var score = highScoreEntry.score;
+        int score = highScoreEntry.score;
         entryTransform.Find("ScoreEntry").GetComponent<Text>().text = score.ToString();
 
         entryTransform.Find("DateEntry").GetComponent<Text>().text = highScoreEntry.Date;
 
         transformlList.Add(entryTransform);
+
+    }
+
+    public void ResetScoreboard()
+    {
+        _entryContainer.gameObject.SetActive(false);
+        _highscoreEntryTransformList.Clear();
+        SaveHighscores(new List<HighscoreEntry>());
+        ShowEntries(LoadHighscores());
+
     }
     [Serializable]
-    private class HighscoreEntry
+    private sealed class Highscores
     {
-        public int score;
-        private string date;
-        public string Date { get => date; set => date = value.Replace('/','.'); }
+        public List<HighscoreEntry> highscoreEntryList;
+        public Highscores(List<HighscoreEntry> highscores)
+        {
+            highscoreEntryList = highscores;
+        }
+    }
+    [Serializable]
+    private sealed class HighscoreEntry
+    {
+        public int score = 0;
+        public string date;
+        public string Date { get => date; set => date = value.Replace('/', '.'); }
     }
 }
